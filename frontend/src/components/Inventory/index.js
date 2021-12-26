f6068d346be1665669c17582fb2152b93e408583
Gautier <73314780+GautierED@users.noreply.github.com>
2021-12-26 11:04:12 +0100

    metamask

--- frontend/src/components/Inventory/index.js
+++ frontend/src/components/Inventory/index.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import {ethers, Contract } from 'ethers';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 const Inventory = () => {
 
@@ -7,7 +9,12 @@ const Inventory = () => {
     useEffect(() => {
 
         const init = async () => {
-
+ 
+            const provider = await detectEthereumProvider();
+            if(provider) {
+                alert('truc')
+            }
+            
         };    
         init();
     }, []);    
