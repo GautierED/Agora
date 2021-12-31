69f6ea757a745cbec8804b4adaca1c70bbb4fc00
GautierED <gautierescallierduront@gmail.com>
2021-12-31 16:19:13 +0100

    getting metadata from each nfts

--- frontend/src/components/Inventory/index.js
+++ frontend/src/components/Inventory/index.js
@@ -2,20 +2,63 @@ import React, { useState, useEffect } from 'react';
 import {ethers, Contract } from 'ethers';
 import detectEthereumProvider from '@metamask/detect-provider';
 
+
+import contractABI from '../../abi.json';
+
+const contractAddress = '0x2aBf143BF98197f1cE3893F882f3b3222d0cFcc9';
+const bscChainId = '97';
+
+const ipfsClient = require("ipfs-http-client");
+/*
+const client = ipfsClient({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https",
+});
+*/
+
 const Inventory = () => {
 
-    const [message, setMessage] = useState('hello')
+    const [message, setMessage] = useState(undefined);
+    const [userAddress, setUserAddress] = useState(undefined);
+    const [chainId, setChainId] = useState(undefined);
+    const [totalSupply, setTotalSupply] = useState(undefined);
 
     useEffect(() => {
-
+        let itemsList = [];
         const init = async () => {
 
-            const provider = await detectEthereumProvider();
+            let provider = await detectEthereumProvider();
+            if(provider){
 
-            if(!provider) alert('Please download Metamask');
+                const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
 
-            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-            alert(accounts[0]);
+                if(accounts[0]){
+
+                    setUserAddress(accounts[0]);
+
+                    let chain = await provider.request({ method: 'eth_chainId' });
+                    chain = String(parseInt(chain, 16));
+                    setChainId(chain);
+
+                    if(chain === bscChainId){
+
+                        provider = new ethers.providers.Web3Provider(provider);
+                        const signer = provider.getSigner();
+
+                        const contract = new Contract(
+                            contractAddress,
+                            contractABI,
+                            signer
+                        );
+
+                        loadInventory(contract, itemsList);
+
+                    } else alert('Please switch to the binance smart chain');
+
+                } else alert('Pease login with Metamask');
+
+            } else alert('Please install Metamask');
             
         };    
         init();
@@ -24,13 +67,37 @@ const Inventory = () => {
     window.ethereum.on('accountsChanged', (_account) => window.location.reload());
     window.ethereum.on('chainChanged', (_chainId) => window.location.reload());
 
-    async function loadInventory(contractAddress, userAddress){
+    async function loadInventory(contract, itemsList){
+        let supply = await contract.totalSupply();
+        supply = parseInt(supply._hex, 16);
+
+        for(var tokenId = 0; tokenId < supply; tokenId++){
+            let ownerAddress = await contract.ownerOf(tokenId);
+            let tokenURI = await contract.tokenURI(tokenId);
+            
+            itemsList.push({
+
+                id:tokenId,
+                owner: ownerAddress,
+                uri:tokenURI,
+
+            });
+
+            let base = "https://ipfs.infura.io:5001/api/v0/cat?arg=";
+            let final = base + tokenURI.substring(7);
+            let output = await fetch(final);
+            let data = await output.text();
+            console.log(data);
+
+        }
 
     };
 
     return (
         <div>
-            {message}
+            {userAddress}<br></br>
+            {chainId}<br></br>
+            {totalSupply}
         </div>
     );
 };
